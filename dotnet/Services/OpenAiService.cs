76420684834e11fab52de6388ee22fe7e52485e7
kyle.easterly <kyle.east@gmail.com>
2023-08-14 21:03:40 -0700

    Added OpenAI API key entry, save key back to appsettings.json for next time.

--- dotnet/Services/OpenAiService.cs
+++ dotnet/Services/OpenAiService.cs
@@ -5,6 +5,18 @@ namespace TSRACT.Services
 {
     public class OpenAiService
     {
+        private string _apiKey;
+        
+        public OpenAiService(IConfiguration configuration)
+        {
+            _apiKey = configuration["OpenAiApiKey"];
+        }
+        
+        public bool IsApiKeySet()
+        {
+            return !string.IsNullOrEmpty(_apiKey);
+        }
+
         public List<ChatConversation> LoadFromPath(string filePath)
         {
             if (string.IsNullOrWhiteSpace(filePath))
@@ -15,6 +27,18 @@ namespace TSRACT.Services
 
             var json = File.ReadAllText(filePath);
             return JsonSerializer.Deserialize<List<ChatConversation>>(json);
+        }
+
+        public async Task SetApiKey(string apiKey)
+        {
+            _apiKey = apiKey;
+
+            // TODO: This is sloppy, should not be doing this from here!
+            var json = File.ReadAllText("appsettings.json");
+            var jsonObj = Newtonsoft.Json.Linq.JObject.Parse(json);
+            jsonObj["OpenAiApiKey"] = apiKey;
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("appsettings.json", output);
         }
     }
 }
